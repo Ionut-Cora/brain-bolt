4a7d280b2aeeb7e6bce86b13772ee4da9724eab9
Ionut Cora <cora.ionut92@yahoo.com>
2026-05-29 22:03:08 +0100

    Add show question function

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,17 @@ const quizSection = document.getElementById("quiz-section");
 const resultSection = document.getElementById("result-section");
 const totalQuestions = document.getElementById("total-questions");
 const scoreText = document.getElementById("score");
+const questionNumber = document.getElementById("question-number");
+const questionImage = document.getElementById("question-image");
+const questionText = document.getElementById("question");
+const answersContent = document.getElementById("answers-content");
+const feedbackSection = document.getElementById("feedback-section");
 
 let questions = [];
 let currentIndex = 0;
 let score = 0;
 let playerName = "";
+let timer;
 
 // Start Quiz
 const startQuiz = async (event) => {
@@ -55,4 +61,41 @@ const getQuestions = async (amount, difficulty) => {
       }
 
   return data.results;
+};
+
+// Show Question
+const showQuestion = async () => {
+  clearInterval(timer);
+  answered = false;
+
+  feedbackSection.classList.add("hidden");
+  answersContent.innerHTML = "";
+
+  if (currentIndex >= questions.length) {
+    endQuiz();
+    return;
+  }
+
+  const item = questions[currentIndex];
+
+  questionNumber.textContent = currentIndex + 1;
+  questionText.textContent = cleanText(item.question);
+
+  questionImage.src = await getImage(item.question, item.correct_answer);
+  questionImage.alt = `Image related to ${cleanText(item.correct_answer)}`;
+
+  const answers = [item.correct_answer, ...item.incorrect_answers];
+
+  shuffle(answers).forEach((answer) => {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "answer-btn";
+    button.textContent = cleanText(answer);
+    button.addEventListener("click", () =>
+      checkAnswer(button, answer, item.correct_answer),
+    );
+    answersContent.appendChild(button);
+  });
+
+  startTimer();
 };
