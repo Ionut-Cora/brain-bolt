da22a296c2672188dcf46b6e8749bb32d2eb748c
Ionut Cora <cora.ionut92@yahoo.com>
2026-05-29 21:45:53 +0100

    Add start quiz js game variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,11 @@
-console.log('Welcome to BrainBolt!');
+// Game Variables
+const startSection = document.getElementById("start-section");
+const startForm = document.getElementById("start-form");
+const playerNameInput = document.getElementById("player-name");
+const questionCountSelect = document.getElementById("question-count");
+const difficultyLevel = document.getElementById("difficulty-level");
+const messageText = document.getElementById("message-text");
+const quizSection = document.getElementById("quiz-section");
+const resultSection = document.getElementById("result-section");
+const totalQuestions = document.getElementById("total-questions");
+const scoreText = document.getElementById("score");
